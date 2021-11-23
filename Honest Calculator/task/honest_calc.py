055a3bfc38bf31f228474a139014b5ac8db7296a
Joshua Meis <67470381+DireMouseRat@users.noreply.github.com>
2021-11-22 19:58:34 -0600

    Finished 3 of 5

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,11 +1,7 @@
-YES = ['yes', 'y', 'Yes', 'Y']
-NO = ['no', 'n', 'No', 'n']
-
-
 class Calculator:
     ask_for_equation = "Enter an equation\n"
-    ask_store_or_discard_result = "Do you want to store the result? (y / n):\n"
-    ask_continue_or_exit = "Do you want to continue calculations? (y / n):\n"
+    ask_store_result = "Do you want to store the result? (y / n):\n"
+    ask_continue_calculations = "Do you want to continue calculations? (y / n):\n"
     input_is_alpha = "Do you even know what numbers are? Stay focused!"
     input_is_not_operator = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
     input_is_division_by_zero = "Yeah... division by zero. Smart move..."
@@ -14,13 +10,16 @@ class Calculator:
         self.x = 0.0
         self.y = 0.0
         self.result = 0.0
+        self.memory = 0.0
         self.operator = ''
-        self.memory = []
         self.start_calculator()
 
     def get_valid_input(self):
-        while True:
+        invalid = True
+        while invalid:
             left, middle, right = input(self.ask_for_equation).split()
+            left = str(self.memory) if left == 'M' else left
+            right = str(self.memory) if right == 'M' else right
             if left.isalpha() or right.isalpha():
                 print(self.input_is_alpha)
             elif middle not in ['+', '-', '*', '/']:
@@ -31,7 +30,7 @@ class Calculator:
                 self.x = float(left)
                 self.y = float(right)
                 self.operator = str(middle)
-                break
+                invalid = False
 
     def calculate_operation(self):
         if self.operator == '+':
@@ -47,19 +46,31 @@ class Calculator:
         print(self.result)
 
     def store_or_discard(self):
-        if input(self.ask_store_or_discard_result) in YES:
-            self.memory.append(self.result)
+        invalid = True
+        while invalid:
+            user_response = input(self.ask_store_result)
+            if user_response == 'y':
+                self.memory = self.result
+                invalid = False
+            elif user_response == 'n':
+                invalid = False
 
     def start_calculator(self):
-        self.get_valid_input()
-        self.calculate_operation()
-        self.display_result()
-        self.store_or_discard()
-        self.continue_or_exit()
+        continue_calculating = True
+        while continue_calculating:
+            self.get_valid_input()
+            self.calculate_operation()
+            self.display_result()
+            self.store_or_discard()
+            continue_calculating = self.continue_calculations()
 
-    def continue_or_exit(self):
-        if input(self.ask_continue_or_exit) in YES:
-            self.start_calculator()
+    def continue_calculations(self):
+        while True:
+            user_response = input(self.ask_continue_calculations)
+            if user_response == 'y':
+                return True
+            elif user_response == 'n':
+                return False
 
 
 Calculator()
